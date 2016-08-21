e8156838d0f3f914f6932d94b3166cc3d1af0888
Cameron <cameron.webley@gmail.com>
2016-08-21 14:30:41 -0500

    feat(index): handling of NaN
    int-to-streak now properly handles strings that can't be coerced into numbers by returning
    
    undefined.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function intToStreak (streakInt) {
-	if (streakInt === 0) {
-		return undefined;
+	if (streakInt > 0) {
+		return `${streakInt}W`;
 	}
 	if (streakInt < 0) {
 		return `${-1 * streakInt}L`;
 	}
-	return `${streakInt}W`;
+	return undefined;
 };
--- src/index.test.js
+++ src/index.test.js
@@ -2,9 +2,6 @@ import { expect } from 'chai';
 import intToStreak from './index';
 
 describe('int-to-streak', () => {
-	it('returns undefined when streak is 0', () => {
-		expect(intToStreak(0)).to.be.undefined;
-	});
 	it('handles winning streaks properly', () => {
 		expect(intToStreak(1)).to.equal('1W');
 		expect(intToStreak(45)).to.equal('45W');
@@ -12,5 +9,12 @@ describe('int-to-streak', () => {
 	it('returns losing streaks properly', () => {
 		expect(intToStreak(-1)).to.equal('1L');
 		expect(intToStreak(-45)).to.equal('45L');
+	});
+	it('returns undefined when streak is 0', () => {
+		expect(intToStreak(0)).to.be.undefined;
+	});
+	it('returns undefined when streak is not coercable into a number', () => {
+		expect(intToStreak(NaN)).to.be.undefined;
+		expect(intToStreak('foo')).to.be.undefined;
 	});
 });
